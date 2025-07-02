9c6be5eb980972e920220b3d30e764d840e886cd
Ryzh <gooddmitriy@mail.ru>
2025-07-02 11:49:53 +0300

    tuples finish

--- lesson_03/mylib/distance.exs
+++ lesson_03/mylib/distance.exs
@@ -32,6 +32,12 @@ defmodule DistanceTest do
     assert FloatExample.equal?(distance({:point, 0, 0}, {:point, 3, 4}), 5.0)
   end
 
+  test "distance crash" do
+    p = {:point, 0, - 16}
+    c = {:circle, {:point, 5, 5}, 20}
+    assert_raise FunctionClauseError, fn -> distance(p, c) end
+  end
+
   test "point inside circle" do
     p1 = {:point, 10, 10}
     p2 = {:point, 10, 100}
@@ -56,5 +62,14 @@ defmodule DistanceTest do
     refute point_inside_figure?(p2, r)
     assert point_inside_figure?(p3, r)
     refute point_inside_figure?(p4, r)
+  end
+
+  test "point inside figure crash" do
+    p = {:point, 5, 5}
+    p1 = {:point, 10, 10}
+    p2 = {:point, 10, 100}
+    p3 = {:point, 0, 0}
+    t = {:triangle, p1, p2, p3}
+    assert_raise FunctionClauseError, fn -> point_inside_figure?(p, t) end
   end
 end
