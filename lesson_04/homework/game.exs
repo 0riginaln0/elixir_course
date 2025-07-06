99e17a49ab86548f3eca0f659fe2f28874b8ddd8
Ryzh <gooddmitriy@mail.ru>
2025-07-06 17:23:03 +0300

    hw 04.1

--- lesson_04/homework/game.exs
+++ lesson_04/homework/game.exs
@@ -1,19 +1,34 @@
 defmodule Game do
+  def join_game({:user, _, _, role}) when role == :admin or role == :moderator, do: :ok
 
-  def join_game(user) do
-    # TODO add your implementation
+  def join_game({:user, _, age, _}) do
+    if age >= 18 do
+      :ok
+    else
+      :error
+    end
   end
 
   def move_allowed?(current_color, figure) do
-    # TODO add your implementation
+    {figure, figure_color} = figure
+
+    if (figure == :pawn or figure == :rock) and current_color == figure_color do
+      true
+    else
+      false
+    end
   end
 
   def single_win?(a_win, b_win) do
-    # TODO add your implementation
+    a_win != b_win
   end
 
   def double_win?(a_win, b_win, c_win) do
-    # TODO add your implementation
+    case {a_win, b_win, c_win} do
+      {true, true, false} -> :ab
+      {true, false, true} -> :ac
+      {false, true, true} -> :bc
+      _ -> false
+    end
   end
-
 end
