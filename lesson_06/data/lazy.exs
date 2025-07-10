87349f760e6b25efda9ea0e5a06b5e46354fec41
Ryzh <gooddmitriy@mail.ru>
2025-07-10 22:08:35 +0300

    pretty html table

---
+++ lesson_06/data/lazy.exs
@@ -0,0 +1,58 @@
+defmodule Lazy do
+  def get_longest_term(file) do
+    File.read!(file)
+    |> String.split("\n")
+    |> Enum.map(fn line -> String.split(line, ":") end)
+    |> Enum.map(fn [term, _] -> term end)
+    |> Enum.map(fn term -> {String.length(term), term} end)
+    |> Enum.max_by(fn {len, _} -> len end)
+    |> elem(1)
+  end
+
+  def get_longest_term_lazy(file) do
+    File.stream!(file)
+    |> Stream.map(fn line -> String.split(line, ":") end)
+    |> Stream.map(fn [term, _] -> term end)
+    |> Stream.map(fn term -> {String.length(term), term} end)
+    |> Enum.max_by(fn {len, _} -> len end)
+    |> elem(1)
+  end
+
+  def test_data do
+    [
+      {"Bob", 24},
+      {"Bill", 25},
+      {"Kate", 26},
+      {"Helen", 34},
+      {"Yury", 16}
+    ]
+  end
+
+  def make_table(data) do
+    styles = Stream.cycle([
+      ~s|style="background-color: #ffffff;"|, # white
+      ~s|style="background-color: #f2f2f2;"|, # gray
+      ])
+    iterator = Stream.iterate(1, fn i -> i + 1 end)
+
+    rows =
+      Stream.zip(styles, iterator)
+      |> Stream.zip(data)
+      |> Enum.map(fn {{style, index}, {name, age}} ->
+        """
+        <tr #{style}>
+          <td>#{index}</td>
+          <td>#{name}</td>
+          <td>#{age}</td>
+        </tr>
+        """
+      end)
+      |> Enum.join("\n")
+
+    """
+    <table style="border-collapse: collapse; text-align: center;" border="1" cellpadding="6">
+    #{rows}
+    </table>
+    """
+  end
+end
